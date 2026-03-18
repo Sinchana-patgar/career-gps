777ec0595117bf9116763d3fc5773995906a7ef6
Sinchana-patgar <sinchanapatgar24@gmail.com>
2026-03-18 19:04:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import streamlit as st
 def main():
     st.set_page_config(page_title="Career GPS", layout="wide")
     st.title("📍 Career GPS")
+    st.write("Use sidebar to navigate")
     st.markdown("Your personal AI mentor to help you navigate your career journey")
 
     if st.button("Get Started 🚀"):
-        st.title("Career GPS App")
-        st.write("Welcome!")  # Will redirect to login/register
+       st.switch_page("pages/auth.py")
 
 if __name__ == "__main__":
     main()
